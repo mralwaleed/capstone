e795d3f66f8906e6d857384e9dd0150457b31b07
mralwaleed <alwaleed_albader@hotmail.com>
2021-09-11 16:57:13 +0300

    change model

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import json
 import os
 
-from sqlalchemy.sql.sqltypes import Integer
+
+from sqlalchemy.sql.sqltypes import Integer, Date
 
 
 database_path = 'postgres://bwzoeiwpvzwgct:022d2874c3dea5f4a70d683308c00242c95568e1bfde5f9942342b0d32f363ef@ec2-44-196-8-220.compute-1.amazonaws.com:5432/dfqafbqm2cva38'
@@ -26,12 +28,19 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
+class Movies(db.Model):  
+  __tablename__ = 'Movies'
 
   id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+  title = Column(String, nullable=False)
+  release_date = Column(Date,  nullable=False)
+
+class Actors(db.Model):  
+  __tablename__ = 'Actors'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String, nullable=False)
+  age = Column(String,  nullable=False)
 
   def __init__(self, name, catchphrase=""):
     self.name = name
